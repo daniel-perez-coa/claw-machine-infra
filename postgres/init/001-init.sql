695a9d02b216ce750b519380e8d834cd82b456c3
Daniel David Perez Coapango <dano.prez.coa@gmail.com>
2026-03-12 19:31:13 -0600

    delete init postgre

--- postgres/init/001-init.sql
+++
@@ -1,9 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS app;
-
-CREATE TABLE IF NOT EXISTS app.healthcheck (
-                                               id SERIAL PRIMARY KEY,
-                                               status VARCHAR(20) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-    );
-
-INSERT INTO app.healthcheck (status) VALUES ('ok');
